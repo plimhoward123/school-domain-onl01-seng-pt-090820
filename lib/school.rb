cdca96f2a8fcef369487b51955d18998ddf8bf80
Plimon Howard V <plimon.howard@gmail.com>
2020-10-05 19:47:03 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,30 @@
-# code here!
+class School
+
+  def initialize(school)
+    @domain = school
+    @roster = Hash.new
+  end
+
+  def roster=()
+    @roster = {}
+  end
+  def roster
+    @roster
+  end
+
+  def add_student(name,grade)
+    if (roster.key?(grade))
+      roster[grade] << name
+    else
+      roster[grade] = [name]
+    end
+  end
+
+  def grade(g)
+    return roster[g]
+  end
+
+  def sort()
+    return roster.sort.to_h
+  end
+end
